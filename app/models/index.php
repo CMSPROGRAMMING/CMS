9caa32e065f66abd9a3c63d3895eada001b075a5
krolik1778 <krol1778@op.pl>
2015-04-15 17:16:11 +0200

    function select

--- app/models/index.php
+++ app/models/index.php
@@ -21,11 +21,28 @@
         AskQuery($GLOBALS['link'], $query);
     }
     
-    $result = DoQuery(Select(tabela,rzecz));
+    function Select($nazwatabeli,$nazwykolumn=array())
+    {
+       $i=0;
+    foreach($nazwykolumn as $klucz)
+        {
+            if ($i == 0){
+            $kolumny = $klucz;
+             $i++;
+            }
+            else { 
+            $kolumny .= ','.$klucz;
+            }
+        }
+        $query="SELECT $kolumny FROM $nazwatabeli";
+        return $query;
+    }
     
-    Select(Tabel,rzecz) = "SELECT rzecz FROM tabela";
+    //$result = DoQuery(Select(tabela,rzecz));
+    
+   // Select(Tabel,rzecz) = "SELECT rzecz FROM tabela";
     
     //LoadCurrentModel();
     
-    //DoQuery($query);
-    
+    DoQuery($query);
+    $result = DoQuery(Select('sys_logs',array('query','user','message')));
