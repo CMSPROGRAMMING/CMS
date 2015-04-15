861d05067f6a50e9b2d536415f756e7bcc1f81f4
Tweester <tweester.pl@gmail.com>
2015-04-15 16:38:06 +0200

    Model Select QUery

--- app/models/index.php
+++ app/models/index.php
@@ -4,11 +4,28 @@
     
     $query = "SELECT * FROM sys_logs";
     echo $query;
+    
+    function LoadCurrentModel()
+    {
+        $current_model = GetCurrentController();
+        echo $current_model;
+        
+        if($current_model != "home")
+        {
+            require_once MODELS_DIR . $current_model. '.php';
+        }
+    }
 
     function DoQuery($query)
     {
         AskQuery($GLOBALS['link'], $query);
     }
     
-    DoQuery($query);
+    $result = DoQuery(Select(tabela,rzecz));
+    
+    Select(Tabel,rzecz) = "SELECT rzecz FROM tabela";
+    
+    //LoadCurrentModel();
+    
+    //DoQuery($query);
     
