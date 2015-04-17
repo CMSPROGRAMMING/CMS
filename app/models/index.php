e992826edfb17aa498587bed57a7c06bff4673c8
krolik1778 <krol1778@op.pl>
2015-04-17 14:21:54 +0200

    Poprawione

--- app/models/index.php
+++ app/models/index.php
@@ -2,12 +2,10 @@
 
     $link = Conection();
     
-<<<<<<< HEAD
     //$query = "SELECT * FROM sys_logs";
     //echo $query;
     
-=======
->>>>>>> origin/master
+
     function LoadCurrentModel()
     {
         $current_model = GetCurrentController();
@@ -32,23 +30,7 @@
     
     function Select($tablename,$columnnames=array(),$mathematicalconditions=array(),$logicalconditions=array())
     {
-<<<<<<< HEAD
-        $i=0;
-        foreach($nazwykolumn as $klucz)
-            {
-                if ($i == 0){
-                $kolumny = $klucz;
-                 $i++;
-                }
-                else { 
-                $kolumny .= ','.$klucz;
-                }
-            }
-            $query="SELECT $kolumny FROM $nazwatabeli";
-            return $query;
-=======
        $i=0;
-
     foreach($columnnames as $key)
         {
             if ($i == 0)
@@ -63,10 +45,9 @@
         }
     if (count($mathematicalconditions)==0)
     {
-        echo "SELECT $column FROM $tablename";
         $query="SELECT $column FROM $tablename";
         return $query;
->>>>>>> origin/master
+
     }
     else
     {
@@ -85,13 +66,12 @@
             }
                 
         }
-        echo "SELECT $column FROM $tablename WHERE $values";
         $query="SELECT $column FROM $tablename WHERE $values";
         return $query;
     }
     }
     
-<<<<<<< HEAD
+
     function ResultExtract($tabel_name, $columns_name=array())
     {
         $result = DoQuery(Select($tabel_name, $columns_name));
@@ -110,27 +90,26 @@
         mysqli_free_result($result);
         return $data_array;
     }
-=======
+
   //Select('sys_logs',array('id','user','message'),array(Condition('id','>','5'),Condition('user','=','Tweester'),Condition('message','=','Poprawne zapytanie')),array('and','or')); 
   //Select('sys_logs',array('id','user','message'),NULL,NULL); 
 
    //$result = DoQuery(Select(tabela,rzecz));
     
    // Select(Tabel,rzecz) = "SELECT rzecz FROM tabela";
->>>>>>> origin/master
-    
+
     //$result = DoQuery(Select('sys_logs',array('query','user','message')));
 
     $data = ResultExtract('sys_logs', array('query','user'));
     
-<<<<<<< HEAD
+
     for($i = 0; $i < count($data); $i++)
     {
         echo "User: " . $data[$i]["user"] . "<br />";
         echo "Query: " . $data[$i]["query"] . "<br />";        
         //echo "Message: " . $data[$i]["message"] . "<br /><br />";
     }
-=======
+
     //DoQuery($query);
     //$result = DoQuery(Select('sys_logs',array('query','user','message')));
->>>>>>> origin/master
+
